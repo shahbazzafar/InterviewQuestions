d2bad05ad3605e307476a4dc68e7e9505536f300
shahbaz <malikshahbazzafar@gmail.com>
2023-12-23 00:33:02 +0500

    first commit

---
+++ app/src/main/java/com/example/interviewquestions/kotlin/scopedfunction/ScopedFunctionsQuestionAndAnswer.kt
@@ -0,0 +1,15 @@
+package com.example.interviewquestions.kotlin.scopedfunction
+
+//(1) How Many types of scoped functions?
+
+//Ans: There are 5 types of scoped functions
+
+//(1) let (2) run (3) with (4) apply (5) also
+
+//(2) apply and also return only object and others return lambda result it can be any thing these return objects also
+
+//(3) Context Of Object: let and also return it and with, run and apply return this
+
+//(4) Context Renaming: We can rename it but cannot rename this
+
+//(5) Easy Null Check: In with we cannot apply null check only else we can
---
+++ app/src/main/java/com/example/interviewquestions/observers/livedata/LiveDataQuestionsAndAnswers.kt
@@ -0,0 +1,15 @@
+package com.example.interviewquestions.observers.livedata
+
+//(1) D/F between observe and observeForever
+
+//(2) Transformation in LiveData
+
+//(3) MediatorLiveData / Nested Observer
+
+//(4) postValue vs setValue
+
+//(5) Single livedata with multiple observers / How its works / Kotlin Callbacks
+
+//(6) Can livedata observe values when app is in background
+
+//(7) if livedata observer is written in any button click listener then it will observe the value automatically or only on button click?
